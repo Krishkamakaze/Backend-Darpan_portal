27fe415da660ec81c1e18f0a55edae90b94a43a8
Krishkamakaze <krishkumar901255@gmail.com>
2024-04-16 19:51:28 +0530

    change in admin

--- Darpan_backend/api/admin.py
+++ Darpan_backend/api/admin.py
@@ -5,4 +5,4 @@ from .models import Student
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ['rollnumber' , 'studentName' , 'branch' , 'programme']
+    list_display = ['rollnumber' , 'studentName' , 'branch' , 'Certificate']
